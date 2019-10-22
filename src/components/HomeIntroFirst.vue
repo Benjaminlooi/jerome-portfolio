396c15a30ce8e7cca458c7daf6f31d05ef58b240
Benjamin Looi <benjaminlooidt@gmail.com>
2019-10-22 20:14:10 +0800

    Update HomeIntroFirst.vue

--- src/components/HomeIntroFirst.vue
+++ src/components/HomeIntroFirst.vue
@@ -155,7 +155,7 @@ export default {
   background-image: url("../assets/DSC00968-min-edited.jpg");
   background-size: cover;
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: 0 50%;
   // -moz-filter: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'><filter id='grayscale'><feColorMatrix type='matrix' values='0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0'/></filter></svg>#grayscale");
   // -o-filter: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'><filter id='grayscale'><feColorMatrix type='matrix' values='0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0'/></filter></svg>#grayscale");
   // -webkit-filter: grayscale(100%);
@@ -229,7 +229,7 @@ export default {
     margin-top: 45px;
   }
 }
-.social-medias .spanses span {
+.spanses span {
   display: inline-block;
   animation: blur 2.5s both, shake 5s 3.5s forwards infinite;
 }
